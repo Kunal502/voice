83f6278005aea3a7fb943054cc2f828908c313f7
Kunal502 <85110718+Kunal502@users.noreply.github.com>
2021-11-05 13:13:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var Recognition = new SpeechRecognition();
+
+
+function start(){
+    document.getElementById("textarea").innerHTML = "";
+    
+}
